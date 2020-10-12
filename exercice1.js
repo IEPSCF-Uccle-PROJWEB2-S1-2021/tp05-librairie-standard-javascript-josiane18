c1a7c071bc548514f2fec6688166205b62ed99fa
mutwarekazi <mutwarekazi.josiane@outlook.fr>
2020-10-12 15:05:30 +0200

    TP05 librairie-standard-javascript

--- exercice1.js
+++ exercice1.js
@@ -1,3 +1,36 @@
 function chuckNorris(msg) {
   return "";
+}let codeBinaire = "";
+
+for (i in msg) {
+  codeBinaire += msg[i].charCodeAt(0).toString([2]) + " ";
 }
+
+let codeUnaire = "";
+let tab = codeBinaire.split("");
+
+if (tab[0] === '1') {
+  codeUnaire += "0 0";
+} else {
+  codeUnaire += "00 0";
+}
+
+for (let a = 1; a < tab.length; a++) {
+  if (tab[a] === tab[(a - 1)]) {
+    codeUnaire += "0";
+  } else {
+    if (tab[a] === '1') {
+      codeUnaire += " 0 0";
+    } else {
+      codeUnaire += " 00 0";
+    }
+  }
+}
+return codeUnaire;
+
+
+console.log(chuckNorris("C"));
+console.log(chuckNorris("CC"));
+
+
+
--- exercice2.js
+++ exercice2.js
@@ -1,3 +1,30 @@
 function analyzeTemperature(quantity, data) {
   return 0;
+  let tabData = data.split(" ");
+
+  if (quantity >= 0 && quantity < 10000) {
+
+    for (let i = 0; i < tabData.length; i++) {
+      if (closest === 0) {
+        closest = tabData[i]
+      } else if (tabData[i] > 0 && tabData[i] <= Math.abs(closest)) {
+        closest = tabData[i]
+      } else if (tabData[i] < 0 && -tabData[i] < Math.abs(closest)) {
+        closest = tabData[i]
+      }
+    }
+    return closest;
+  }else{
+    return "La quantite doit etre comprise entre 0 et 10 000 max";
+  }
 }
+
+console.log(analyzeTemperature(1000, '1 -2 -8 4 5'));
+
+
+
+
+
+
+
+
